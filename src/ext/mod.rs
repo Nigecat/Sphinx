add924138cc137bc8bb15afbd57da7359373c999
Nigecat <development@nigelq.com>
2022-05-14 11:05:53 +1000

    Add ui extension trait

--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,5 +1,7 @@
 //! A collection of traits which extend specific internal types.
 
 mod response;
+mod ui;
 
 pub use response::ResponseExt;
+pub use ui::UiExt;
---
+++ src/ext/ui.rs
@@ -0,0 +1,30 @@
+use crate::{Response, Ui, Vec2, Widget};
+
+/// Extends a [`crate::Ui`].
+pub trait UiExt {
+    /// Add an element using both [`eframe::egui::Ui::add_sized`] and [`eframe::egui::Ui::add_enabled`].
+    fn add_enabled_with_size(
+        &mut self,
+        enabled: bool,
+        size: impl Into<Vec2>,
+        widget: impl Widget,
+    ) -> Response;
+}
+
+impl UiExt for Ui {
+    fn add_enabled_with_size(
+        &mut self,
+        enabled: bool,
+        size: impl Into<Vec2>,
+        widget: impl Widget,
+    ) -> Response {
+        if self.is_enabled() && !enabled {
+            self.set_enabled(false);
+            let response = self.add_sized(size, widget);
+            self.set_enabled(true);
+            response
+        } else {
+            self.add_sized(size, widget)
+        }
+    }
+}
